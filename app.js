b8503d04204c5de0ddb8762605e2aa5dd2c3554d
astevens104 <agstevens104@gmail.com>
2024-07-15 13:18:22 -0700

    more intermediate js practice

--- app.js
+++ app.js
@@ -15,8 +15,59 @@
 
 // Q 2 - return the legnth of an array
 
-function arrLength(length) {
-    return (arr.length);
+// function arrLength(length) {
+
+//     return (arr.length);
+// }
+// let arr = []
+// console.log(arrLength(length))
+
+// cleaner method
+// function arrLength(arr) {
+//     return arr.length;
+// }
+
+// console.log(arrLength([1, 2, 3, 4]))  
+
+// Q 3 - get the last element in an array
+
+// function lastElem(arr) {
+//     return arr[arr.length - 1]
+// }
+
+// console.log(lastElem([null, 5, false]))
+
+// q 4 Find the sum of an array
+
+// solution from stackOverflow:
+// const sum = [0, -5, -10].reduce((partialSum, a) => partialSum + a, 0);
+
+// console.log(sum);
+
+// using a for loop:
+
+// function arrSum(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; ++i) {
+//         // this is how you loop thru every element in an array
+//         // console.log(arr[i]);
+//         sum = sum + arr[i];
+//     }
+    
+//     return sum;
+// }
+
+// console.log(arrSum([1, 2, 3, 4, 5, 6]));
+
+// Q 5 - Add up the numbers from a single number
+
+function progressiveSum(num) {
+    let sum = 0
+    for (let i = 0; i < num + 1; ++i) {
+        console.log(i)
+        sum = sum + i
+    }
+    return sum;
 }
 
-console.log(arrLength[1, 2, 3])
+console.log(progressiveSum(5));
